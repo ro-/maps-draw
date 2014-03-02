5e8d8e37da5da88f1afcb8a9af3e8d7e8f45556b
Ro Lewis <ratboystylez@gmail.com>
2014-03-03 00:14:39 +1100

    hmm

---
+++ maps-draw/scripts/maps-draw/types/eventable.js
@@ -0,0 +1,44 @@
+﻿//Shape: base class
+define(
+    [],
+    function () {
+
+        function Eventable() {
+            var _this = this;
+            _this.events = {};
+
+            Eventable.prototype.addEventListener = function (name, handler) {
+                if (_this.events.hasOwnProperty(name))
+                    _this.events[name].push(handler);
+                else
+                    _this.events[name] = [handler];
+            };
+
+            Eventable.prototype.removeEventListener = function (name, handler) {
+                /* This is a bit tricky, because how would you identify functions?
+                   This simple solution should work if you pass THE SAME handler. */
+                if (!_this.events.hasOwnProperty(name))
+                    return;
+
+                var index = _this.events[name].indexOf(handler);
+                if (index != -1)
+                    _this.events[name].splice(index, 1);
+            };
+
+            Eventable.prototype.fireEvent = function (name, args) {
+                if (!_this.events.hasOwnProperty(name))
+                    return;
+
+                if (!args || !args.length)
+                    args = [];
+
+                var evs = _this.events[name], l = evs.length;
+                for (var i = 0; i < l; i++) {
+                    evs[i].apply(null, args);
+                }
+            };
+        }
+
+        return Eventable;
+    }
+);
